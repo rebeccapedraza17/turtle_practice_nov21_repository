1e84b7770f76a2eb7b23b837df1155d388f071e8
rebeccapedraza17 <rebepedraza17@gmail.com>
2024-11-21 12:46:09 -0700

    Add files via upload

---
+++ turtle_practice.py
@@ -0,0 +1,20 @@
+# Rebecca Pedraza
+# Nov 21
+
+import turtle
+'''
+turtle.color("green")
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.speed(0)
+'''
+
+turtle.forward(100)
+turtle.backward(200)
+turtle.left(45)
+turtle.
